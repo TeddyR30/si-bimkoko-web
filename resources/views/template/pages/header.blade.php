9154dc8540c870065ae7f0ac1fbd1649bb191351
TeddyR30 <Synyster1213@gmail.com>
2024-05-30 01:24:41 +0700

    sidebar hover

--- resources/views/template/pages/header.blade.php
+++ resources/views/template/pages/header.blade.php
@@ -2,7 +2,7 @@
     <nav class="navbar navbar-expand-lg navbar-light">
       <ul class="navbar-nav">
         <li class="nav-item d-block d-xl-none">
-          <a class="nav-link sidebartoggler nav-icon-hover" id="headerCollapse" href="javascript:void(0)">
+          <a class="nav-link sidebartoggler nav-icon-hover" id="headerCollapse">
             <i class="ti ti-menu-2"></i>
           </a>
         </li>
@@ -54,7 +54,6 @@
             background: radial-gradient(#3DA3C3 0%, rgba(47, 136, 255, 0.12) 90%, rgba(47, 136, 255, 0.12) 100%);
             border-radius: 5px;
             width: 80%;
-            height: 200%;
         }
       </style>
   </header>
