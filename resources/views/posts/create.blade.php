8e89a2cdc601873f3b94f937cb02ab46f28c22ed
TeddyR30 <Synyster1213@gmail.com>
2024-06-02 00:04:34 +0700

    Bimbingan Edit

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,10 +1,10 @@
 @extends('posts.layout')
 
 @section('content')
-<div class="row">
+<div class="row mb-2 mt-2">
     <div class="col-lg-12 margin-tb">
         <div class="pull-left">
-            <h1 style="text-align: center; background-color: silver; border-radius: 10px">Bimbingan Karir | Tambah</h1>
+            <h1 style="text-align: center; background-color: silver; border-radius: 10px">Bimbingan Karir | Create</h1>
         </div>
         <div class="pull-right">
             <a class="btn btn-primary" href="{{ route('posts.index') }}"> Back</a>
@@ -28,19 +28,19 @@
 
      <div class="row">
         <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Name:</strong>
-                <input type="text" name="name" class="form-control" placeholder="Name">
+            <div class="form-group mb-3 mt-2">
+                <strong>Title :</strong>
+                <input type="text" name="name" class="form-control" placeholder="Title">
             </div>
         </div>
         <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Detail:</strong>
+            <div class="form-group mb-3 mt-2">
+                <strong>Detail :</strong>
                 <textarea class="form-control" style="height:150px" name="detail" placeholder="Detail"></textarea>
             </div>
         </div>
         <div class="col-xs-12 col-sm-12 col-md-12 text-center">
-                <button type="submit" class="btn btn-primary">Submit</button>
+                <button type="submit" class="btn btn-primary mb-2 mt-3">Submit</button>
         </div>
     </div>
 
